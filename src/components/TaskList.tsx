35dbc5de96e5f2188ab741368f5a576a288da099
edumarotta <47776453+edumarotta@users.noreply.github.com>
2021-03-10 08:26:18 -0300

    Final do Desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -15,15 +15,41 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle === '')
+      return;
+    else {
+      setTasks([
+        ...tasks,
+        {
+          id: Math.floor(Math.random() * 100),
+          title: newTaskTitle,
+          isComplete: false
+        }
+      ])
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskToComplete = tasks.findIndex((item: Task) => item.id === id);
+
+    const newTasks = tasks.map((item: Task, index: number) => {
+      if(index === taskToComplete){
+        return {
+          ...item,
+          isComplete: !item.isComplete
+        }
+      } else {
+        return item
+      }
+    })
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((item: Task )=> item.id !== id);
+ 
+    setTasks(newTasks);
   }
 
   return (
